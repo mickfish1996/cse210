a8f7ac80c64927a350e11d5402dd81fb5a859e24
mickfish1996 <fis17005@byui.edu>
2021-10-26 12:35:16 -0600

    got snake running

--- snake/game/food.py
+++ snake/game/food.py
@@ -6,10 +6,25 @@ from game.point import Point
 # TODO: Define the Food class here...
 class Food(Actor):
     def __init__(self):
-        self.__points = 0
-        self.start()
+        super().__init__()
 
-    def start(self):
-        self.__points = random.randint(1,10)
+        self.__points = 0
+        self.reset()
+
+        self.set_height(constants.DEFAULT_SQUARE_LENGTH)
+        self.set_width(constants.DEFAULT_SQUARE_LENGTH)
+
+    def set_points(self,points):
+        self.__points = points
+
+    def get_points(self):
+        return self.__points
+    def reset(self):
+        self.set_points(random.randint(1,10))
+        self.set_text(str(self.get_points()))
+        x = random.randint(1, constants.MAX_X -2)
+        y = random.randint(1, constants.MAX_Y -2)
+        location = Point(x,y)
+        self.set_position(location)
+
     
-    def get_position()
